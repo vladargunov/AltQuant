5926b809f9cdd933db236a08d05a40f4b9192c13
v-argunov <argunovvlad5@gmail.com>
2023-03-02 14:49:44 +0300

    correct config and docs

--- altquant/simulator/trade_index.py
+++ altquant/simulator/trade_index.py
@@ -6,12 +6,12 @@ from dataclasses import dataclass
 from typing import List
 
 
-
 @dataclass
 class TradeDate:
     """
     Dataclass containing a TradeDate used by an Engine
     """
+
     year: int
     month: int
     day: int
@@ -22,71 +22,72 @@ class TradeDate:
 
         if self.month < 0 or self.month > 12:
             raise ValueError("Month value is incorrect!")
-        
+
         if self.year < 0:
             raise ValueError("Year value is incorrect!")
 
 
+class TradeIndex:
+    AVAILABLE_DATE_FORMATS = frozenset(["YYYY-MM-DD"])
 
-class TradeIndex():
-    AVAILABLE_DATE_FORMATS = frozenset(['YYYY-MM-DD'])
     def __init__(self):
         self.dates = None
 
-    def _set_dates(self, dates : List[str], format :str='YYYY-MM-DD'):
+    def _set_dates(self, dates: List[str], format: str = "YYYY-MM-DD"):
         if format not in self.AVAILABLE_DATE_FORMATS:
-            raise KeyError('Such date format is not supported!\n' + 
-                           f'Avaialble date formats are {self.AVAILABLE_DATE_FORMATS}')
-        
-        self.dates = tuple([
-            TradeDate(**self._process_date(date, format)) for date in dates
-            ])
+            raise KeyError(
+                "Such date format is not supported!\n"
+                + f"Avaialble date formats are {self.AVAILABLE_DATE_FORMATS}"
+            )
 
+        self.dates = tuple(
+            [TradeDate(**self._process_date(date, format)) for date in dates]
+        )
 
     def _process_date(self, date, format):
         check_idx = 0
         while check_idx < len(format):
-            if format[check_idx] == 'Y':
+            if format[check_idx] == "Y":
                 try:
-                    year = int(date[check_idx:check_idx + 4])
+                    year = int(date[check_idx : check_idx + 4])
                     check_idx += 4
                 except ValueError:
-                    print(f'Year of date {date} is specified incorrectly ' + 
-                          f'from the given format {format}!')
-            elif format[check_idx] == 'M':
+                    print(
+                        f"Year of date {date} is specified incorrectly "
+                        + f"from the given format {format}!"
+                    )
+            elif format[check_idx] == "M":
                 try:
-                    month = int(date[check_idx:check_idx + 2])
+                    month = int(date[check_idx : check_idx + 2])
                     check_idx += 2
                 except ValueError:
-                    print(f'Month of date {date} is specified incorrectly ' + 
-                          f'from the given format {format}!')
-            elif format[check_idx] == 'D':
+                    print(
+                        f"Month of date {date} is specified incorrectly "
+                        + f"from the given format {format}!"
+                    )
+            elif format[check_idx] == "D":
                 try:
-                    day = int(date[check_idx:check_idx + 2])
+                    day = int(date[check_idx : check_idx + 2])
                     check_idx += 2
                 except ValueError:
-                    print(f'Day of date {date} is specified incorrectly ' + 
-                          f'from the given format {format}!')
+                    print(
+                        f"Day of date {date} is specified incorrectly "
+                        + f"from the given format {format}!"
+                    )
             else:
-                raise ValueError('Internal error regarding date format!')
-
+                raise ValueError("Internal error regarding date format!")
 
             # Check date separator
             if check_idx < len(format):
-                self._check_date_separator(date=date,
-                                        date_sep=date[check_idx],
-                                        format_sep=format[check_idx])
+                self._check_date_separator(
+                    date=date, date_sep=date[check_idx], format_sep=format[check_idx]
+                )
                 check_idx += 1
-        return {'year' : year, 'month' : month, 'day' : day}
-            
-            
-        
+        return {"year": year, "month": month, "day": day}
 
-    def _check_date_separator(self, date : str, date_sep : str, format_sep : str):
+    def _check_date_separator(self, date: str, date_sep: str, format_sep: str):
         if date_sep != format_sep:
-            raise ValueError(f'Separator of date {date} is specified incorrectly' + 
-                             'from the given format!')
-
-
-
-            
+            raise ValueError(
+                f"Separator of date {date} is specified incorrectly"
+                + "from the given format!"
+            )
--- tests/test_trade_index.py
+++ tests/test_trade_index.py
@@ -1,15 +1,17 @@
 from altquant.simulator.trade_index import TradeDate, TradeIndex
-import pytest
+
 
 def test_index_creation():
     """Tests the creation of the TradeIndex class"""
     ti = TradeIndex()
 
-    ti._set_dates(['2021-01-01', '2021-01-02', '2021-01-03'], format='YYYY-MM-DD')
+    ti._set_dates(["2021-01-01", "2021-01-02", "2021-01-03"], format="YYYY-MM-DD")
 
-    check_trade_index_dates = tuple([
-        TradeDate(year=2021, month=1, day=1),
-        TradeDate(year=2021, month=1, day=2),
-        TradeDate(year=2021, month=1, day=3)
-    ])
-    assert ti.dates == check_trade_index_dates, 'TradeIndex yields incorrect dates!'
+    check_trade_index_dates = tuple(
+        [
+            TradeDate(year=2021, month=1, day=1),
+            TradeDate(year=2021, month=1, day=2),
+            TradeDate(year=2021, month=1, day=3),
+        ]
+    )
+    assert ti.dates == check_trade_index_dates, "TradeIndex yields incorrect dates!"
